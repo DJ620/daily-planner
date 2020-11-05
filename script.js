4295e06ac07120231f339d232586caea43915fe4
DJ Hersh <DanielJHersh@gmail.com>
2020-11-05 09:56:34 -0800

    Figured out how to compare the current time to my time variable

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log(moment().format("dddd, MMMM Do, YYYY"));
 $("document").ready(function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-    console.log(moment().get('hour'));
     for (var i = 0; i < 9; i++) {
+
         var timeBlock = $("<section>");
         timeBlock.addClass("time-block");
         var row = $("<div>");
@@ -10,16 +10,18 @@ $("document").ready(function() {
         var hour = $("<div>");
         hour.addClass("hour");
         var current = moment().get('hour');
-        var time = moment().hour(20+i);
+        var time = moment().hour(9+i);
         console.log(moment(time).hour()>current);
         hour.text(time.format('hA'));
         var textarea = $("<textarea>");
-        if (moment(time).isSame(current)) {
+        console.log(time.get('hour'));
+        console.log(current);
+        if (moment(time.get('hour')).isSame(current)) {
             textarea.addClass("present");
-        } else if (moment(time).isBefore(current)) {
-            textarea.addClass("future");
-        } else {
+        } else if (moment(time.get('hour')).isBefore(current)) {
             textarea.addClass("past");
+        } else {
+            textarea.addClass("future");
         }
         textarea.attr("rows", "2");
         var save = $("<button>");
